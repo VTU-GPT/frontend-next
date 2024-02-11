15fc0472795360edacfca2ce3fc9d40fc5be844a
Vinayak Nawdhar <vinayaknawdhar003@gmail.com>
2024-02-11 12:10:29 +0530

    Update AnswerPage.jsx

--- components/AnswerPage.jsx
+++ components/AnswerPage.jsx
@@ -6,7 +6,7 @@ import { handleAsk } from '@/utils/handleAsk.js'
 import { useState } from 'react';
 import ReactLoading from 'react-loading';
 import Source from '@/components/ui/Sources';
-import Skeleton from '@/components/ui/skeleton';
+import Skeleton from '@/components/ui/Skeleton';
 
 
 const AnswerPage = () => {
