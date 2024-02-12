b7bf6e37cfd945a8c074e8a8a96895acb398750d
tejassp03 <tejassp03@gmail.com>
2024-02-12 10:02:41 +0530

    RAG Basic Implementation

--- pages/api/llm.js
+++ pages/api/llm.js
@@ -1,11 +1,15 @@
 // pages/api/llm.js
-
+"use client"
+import { CohereEmbeddings } from "@langchain/cohere";
 import OpenAI from "openai";
-
+import { FaissStore } from "@langchain/community/vectorstores/faiss";
+import { RetrievalQAChain } from "langchain/chains";
 
 const anyscale = new OpenAI({
     baseURL: "https://api.endpoints.anyscale.com/v1",
-    apiKey: "esecret_u2sbcvxgegri9x31reyagyurgb"
+    apiKey: "esecret_u2sbcvxgegri9x31reyagyurgb",
+    modelName:"Open-Orca/Mistral-7B-OpenOrca",
+    temperature:0.7
   });
   
 export default async function handler(req, res) {
@@ -20,16 +24,46 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: "Invalid request body" });
     }
 
+    const embeddings = new CohereEmbeddings({
+      apiKey: "PtVIxuqTBJYMP1GAhjsXbFso9hX08lGVZjIcwBVT", 
+      batchSize: 48, 
+    });
+
+    const loadedVectorStore = await FaissStore.load(
+      "pages/api/vectordb",
+      embeddings
+    );
+
+    
+    // const vectorStoreRetriever = loadedVectorStore.asRetriever();
+    // const chain = RetrievalQAChain.fromLLM(anyscale.chat.completions, vectorStoreRetriever);
+    // const theResponse = await chain.invoke({
+    //   query:question,
+    // });
+
+    const alldata = await loadedVectorStore.similaritySearch(question,3);
+    const pdfSourcesAndPageNumbers = alldata.map(item => ({
+      source: item.metadata.source,
+      pageNumber: item.metadata.loc.pageNumber
+  }));
+  
+  console.log(JSON.stringify(pdfSourcesAndPageNumbers, null, 2));
+    let context = ""; // Initialize context as an empty string
+
+    alldata.forEach((item, i) => {
+        context = context + item.pageContent;
+    });
+
     const completion = await anyscale.chat.completions.create({
         model: "Open-Orca/Mistral-7B-OpenOrca",
-        messages: [{"role": "system", "content": "You are a professional educational assistant. Get to the answer directly without any introduction about yourself or any extra content that is not necessary."}, 
-                   {"role": "user", "content": question}],
+        messages: [{"role": "system", "content": `You are a professional educational assistant. Get to the answer directly without any introduction about yourself or any extra content that is not necessary context: ${context} `},
+        {"role": "user", "content": question}],
         temperature: 0.7
     });
 
     const theResponse = completion.choices[0].message;
-    console.log(theResponse)
-    res.status(200).json({ answer: theResponse });
+    // console.log(theResponse)
+    res.status(200).json({ answer: theResponse,sources: pdfSourcesAndPageNumbers });
   } catch (error) {
     console.error("Error:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
