14ae4ee103e53059fdfc6a4ab829b3c4532ddbd2
Vinayak Nawdhar <vinayaknawdhar003@gmail.com>
2024-02-22 18:37:25 +0530

    Update PdfViewer.jsx

--- components/PdfViewer.jsx
+++ components/PdfViewer.jsx
@@ -1,16 +1,23 @@
 'use client'
 import React from 'react'
 import { Worker } from '@react-pdf-viewer/core';
-import { Viewer } from '@react-pdf-viewer/core';
+import { Viewer,ZoomEvent  } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 
 
 const pdfViewer = ({fileName,pageNumber}) => {
+
+    const handleZoom = (e) => {
+        console.log(`Zoom to ${e.scale}`);
+    };
+
     return (
         <>
+            <div className='w-full overflow-hidden mx-auto'>
             <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-            <Viewer fileUrl={`/documents/${fileName}`} initialPage={pageNumber-1} />;
+            <Viewer fileUrl={`/documents/${fileName}`} initialPage={pageNumber-1} defaultScale={1} onZoom={handleZoom} />;
             </Worker>
+            </div>
         </>
     )
 }
