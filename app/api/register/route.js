a9efa9dcd409212316d2c8894b47325d3a65d21a
Vinayak Nawdhar <vinayaknawdhar003@gmail.com>
2024-04-25 12:10:04 +0530

    Update route.js

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -12,13 +12,13 @@ export async function POST(req,res){
          phone : resp.phone,
          college : resp.college,
          city : resp.city,
-         sem : resp.semester,
+         sem : +resp.semester,
          branch : resp.branch,
-         passing_year : resp.passingYear,
+         passing_year : +resp.passingYear,
          scheme : resp.scheme,
          uid : resp.userId
       }
    })
-   // const sqlResponse =  await sql`insert into users (email,name,phone,college,city,sem,branch,passing_Year,scheme,uid) values (${resp.email},${resp.name},${resp.phone},${resp.college},${resp.city},${resp.semester},${resp.branch},${resp.passingYear},${resp.passingYear},${resp.userId})`
+   const sqlResponse =  await sql`insert into users (email,name,phone,college,city,sem,branch,passing_Year,scheme,uid) values (${resp.email},${resp.name},${resp.phone},${resp.college},${resp.city},${resp.semester},${resp.branch},${resp.passingYear},${resp.passingYear},${resp.userId})`
    return NextResponse.json({response});
 }
